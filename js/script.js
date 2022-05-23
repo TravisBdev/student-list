ee2980e258a4f524c74b9887bc46aa2c4f693c1f
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-05-23 12:12:13 -0400

    completed

--- js/script.js
+++ js/script.js
@@ -1,30 +1,64 @@
 /*
+Author: Travis Brown
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
 
+let itemsPerPage = 9;
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+const showPage = function(list, page){
+    let startIndex = page * itemsPerPage - itemsPerPage;
+    let endIndex = page * itemsPerPage;
+    let studentList = document.querySelector('.student-list');
+    studentList.innerHTML = '';
+    for(let i = 0; i < list.length; i++){
+        if(i >= startIndex && i < endIndex){
+            let studentItem = `
+            <li class="student-item cf">
+                <div class="student-details">
+                    <img class='avatar' src='${list[i].picture.thumbnail}' alt='profile picture'>
+                    <h3>${list[i].name.title} ${list[i].name.first} ${list[i].name.last}</h3>
+                    <span class='date'> ${list[i].registered.date}</span>
+                </div>
+            </li>`;
+            studentList.insertAdjacentHTML("beforeend", studentItem);
+
+        }
+    }
+    
+}
 
 
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+const addPagination = function(list){
+    let numOfPages = Math.ceil(list.length / itemsPerPage);
+    let linkList = document.querySelector('.link-list');
+    linkList.innerHTML = '';
+    for(let i = 1; i < numOfPages; i++){
+        let button = `
+            <li>
+                <button type='button'>${i}</button>
+            </li>`;
+        linkList.insertAdjacentHTML('beforeend', button);
+    }
+
+    let firstButton = linkList.querySelector("button");
+    firstButton.className = "active";
+    linkList.addEventListener('click', (e) => {
+        if(e.target.tagName === 'BUTTON'){
+            let btnActive = document.querySelector('.active');
+            btnActive.className = '';
+            e.target.className = 'active';
+            showPage(list, e.target.textContent);
+        }
+    })
+}
 
 
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
 
 
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
